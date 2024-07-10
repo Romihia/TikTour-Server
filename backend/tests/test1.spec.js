efc77bc3dd9c803b824df5a2066ef682bc96b7ac
Romihia <118122586+Romihia@users.noreply.github.com>
2024-07-10 16:05:44 +0300

    Dividing the project into backend & frontend

---
+++ backend/tests/test1.spec.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('GET /', () => {
+  it('should return Hello World!', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.text.should.equal('Hello World!');
+        done();
+      });
+  });
+});
