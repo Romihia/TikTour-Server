7e7d2ad26d3d96f120f2d3118f35f08cf6c855f3
Romihia <118122586+Romihia@users.noreply.github.com>
2024-07-12 18:35:49 +0300

    Beginning of building the backend, creating mongodb, registration and login

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -4,9 +4,9 @@ import globals from "globals";
 export default [
   {languageOptions: { globals: globals.browser },
   rules:{
-    indent: ['error', 4],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
+    // indent: ['error', 4],
+    // quotes: ['error', 'single'],
+    // semi: ['error', 'always'],
     }
   },
 ];
