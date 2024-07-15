7113b6a30906c62b27e1295c83b1e72ced4f7ef1
Romihia <118122586+Romihia@users.noreply.github.com>
2024-07-15 19:09:14 +0300

    fix fontend

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,10 +1,8 @@
-// // test/App.test.js
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+// test/App.test.js
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '../src/App'; 
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders the app without crashing', () => {
+  render(<App />);
+});
