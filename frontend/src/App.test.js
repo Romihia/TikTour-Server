e9ef6488e5bcacfe846407a764e7c2702b8232a8
Romihia <118122586+Romihia@users.noreply.github.com>
2024-07-15 19:39:50 +0300

    Deleted and moved the frontend to a new repo

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-// test/App.test.js
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from '../src/App'; 
-
-test('renders the app without crashing', () => {
-  render(<App />);
-});
