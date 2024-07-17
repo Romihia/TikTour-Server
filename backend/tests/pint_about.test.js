1499b72d40d792de34c5cdd39aa121499f53b681
Romihia <118122586+Romihia@users.noreply.github.com>
2024-07-17 11:59:25 +0300

    Full repair of the backend, rearrangement of the db, adding routers, adding post, adding tests, adding new rules in eslint.

--- backend/tests/pint_about.test.js
+++ backend/tests/pint_about.test.js
@@ -1,11 +1,11 @@
-import http from 'http';
-import assert from 'assert';
+// import http from 'http';
+// import assert from 'assert';
 
-describe('Ping Endpoint', () => {
-  it('should return 200 OK', (done) => {
-    http.get('http://localhost:3001/ping', (res) => {
-      assert.strictEqual(res.statusCode, 200);
-      done();
-    });
-  });
-});
+// describe('Ping Endpoint', () => {
+//   it('should return 200 OK', (done) => {
+//     http.get('http://localhost:3001/ping', (res) => {
+//       assert.strictEqual(res.statusCode, 200);
+//       done();
+//     });
+//   });
+// });
