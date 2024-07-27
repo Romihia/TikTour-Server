c82097ab57dec98c32f173eaca55f20bcf4b7db4
maxim2100 <maxsim2100@gmail.com>
2024-07-27 20:42:50 +0300

    Update getUser function and add deleteUser function

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -6,9 +6,12 @@ export const getUser = async (req, res) => {
   try {
     const { id } = req.params;
     const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
     res.status(200).json(user);
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
 };
 
@@ -114,5 +117,16 @@ export const updateUserPicture = async (req, res) => {
     res.status(200).json(updatedUser);
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+};
+/* DELETE USER */
+export const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByIdAndDelete(id);
+    if (!user) return res.status(404).json({ msg: "User not found" });
+    res.status(200).json({ msg: "User deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 };
