7e43c67b4829e1695638f8717247563716f4ba19
maxim2100 <maxsim2100@gmail.com>
2024-07-29 19:01:49 +0300

    change the name and the concept of sending emails ,add activation email sender

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,40 @@
+// backend/utils/sendEmail.js
+import nodemailer from 'nodemailer';
+
+
+const sendEmail = async (email, resetLink, type) => {
+  let subject = "";
+  let text = "";
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+  
+  if (type === 'PasswordReset') {
+    subject = "Password Reset";
+    text = `Click the following link to reset your password: ${resetLink}`;
+  } else if (type === 'Activation') { 
+    subject = "Account Activation";
+    text = `Click the following link to activate your account: ${resetLink}`;
+  }
+
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: email,
+    subject: subject,
+    text: text,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log(`${type} email sent`);
+  } catch (error) {
+    console.error(`Error sending ${type.toLowerCase()} email(${email}):`, error);
+    throw new Error(`Error sending ${type.toLowerCase()} email`);
+  }
+};
+
+export default sendEmail;
